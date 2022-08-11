9ec4f7e5c929b4509d257edd616f5ad5a7e00a89
Haipeng Lin <jimmie.lin@gmail.com>
2022-08-11 17:20:50 -0400

    Now generate for WRF-GC as well, merging the 4-years old wrf-gchp-dev-tools

--- generate_species_coupling.py
+++ generate_species_coupling.py
@@ -9,11 +9,57 @@
 import re
 from operator import itemgetter
 
-# choose parser_mode: "regex" for 13.4.x, or "yaml" for 14.1.0+
-parser_mode = "regex"
+# User configurable options:
 
 # choose input file:
-input_file_name = "v13.4.1.dat"
+input_file_name = "v14.0.0.yml"
+
+# cesm | wrfgc
+mode = "wrfgc"
+
+# choose parser_mode: "regex" for 13.4.x, or "yaml" for 14.1.0+
+parser_mode = "yaml" if "yml" in input_file_name else "regex"
+
+# is version 14 and above? used for WRF-GC
+# if yes, uses the new format (State_Chm%Species(id_Hg2)%Conc(:,:,:)) for State_Chm%
+is_gc_14_and_above = "14" in input_file_name
+
+#
+# These are "quirky" species, which necessitate special handling
+# in coupled models such as WRF and CESM.
+#
+
+# Non-gas-phase species ("Aerosols")
+nongas = ["AERI", "ASOAN", "ASOA1", "ASOA2", "ASOA3", "ASOG1", "ASOG2", "ASOG3", "AONITA", "BCPI", "BCPO", "BrSALA", "BrSALC", "DMS", "DST1", "DST2", "DST3", "DST4", "INDIOL", "IONITA", "ISALA", "ISALC", "ISN1OA", "ISN1OG", "ISOA1", "ISOA2", "ISOA3", "MONITA", "MSA", "NH4", "NIT", "NITs", "OCPI", "OCPO", "OPOA1", "OPOA2", "OPOG1", "OPOG2", "SALA", "SALC", "SALACl", "SALAAL", "SALCAL", "SALCCl", "SO4", "SO4s", "SOAIE", "SOAGX", "SOAME", "SOAMG", "SOAP", "SOAS", "TSOA0", "TSOA1", "TSOA2", "TSOA3", "TSOG0", "TSOG1", "TSOG2", "TSOG3", "pFe", "POA1", "POA2", "POG1", "POG2"]
+
+# ComplexSOA
+complexsoa = ["ASOA1", "ASOA2", "ASOA3", "ASOAN", "ASOG1", "ASOG2", "ASOG3", "TSOA0", "TSOA1", "TSOA2", "TSOA3", "TSOG0", "TSOG1", "TSOG2", "TSOG3"]
+complexsoa_svpoa = ["POA1", "POA2", "POG1", "POG2", "OPOA1", "OPOA2", "OPOG1", "OPOG2"]
+
+###################
+#  --- CESM ---   #
+###################
+# CESM has a few fixed species that need to be written to inv(ariants) list
+# from gckpp_Parameters.F90, this is H2, N2, O2, RCOOH
+cesm_invariants = ["H2", "N2", "O2", "RCOOH"]
+
+# CESM-GC requires extra species for aerosols
+cesm_aer = ['bc_a1','bc_a4','dst_a1','dst_a2','dst_a3','ncl_a1','ncl_a2','ncl_a3','num_a1','num_a2','num_a3','num_a4','pom_a1','pom_a4','so4_a1','so4_a2','so4_a3','soa1_a1','soa1_a2','soa2_a1','soa2_a2','soa3_a1','soa3_a2','soa4_a1','soa4_a2','soa5_a1','soa5_a2','H2SO4','SOAG0','SOAG1','SOAG2','SOAG3','SOAG4']
+cesm_aer_mass = [12.011000, 12.011000, 135.064039, 135.064039, 135.064039, 58.442468, 58.442468, 58.442468, 1.007400, 1.007400, 1.007400, 1.007400, 12.011000, 12.011000, 115.107340, 115.107340, 115.107340, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000, 98.078400, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000]
+
+###################
+# --- WRF-GC ---  #
+###################
+# WRF requires non-gas species to be separated into the end. use nongas.
+
+# WRF-GC v2.0+ (Feng et al., 2021) also requires diagnostic bins for aerosols
+wrf_extra_coupling = "diag_so4_a1,diag_so4_a2,diag_so4_a3,diag_so4_a4,diag_nit_a1,diag_nit_a2,diag_nit_a3,diag_nit_a4,diag_nh4_a1,diag_nh4_a2,diag_nh4_a3,diag_nh4_a4,diag_ocpi_a1,diag_ocpi_a2,diag_ocpi_a3,diag_ocpi_a4,diag_ocpo_a1,diag_ocpo_a2,diag_ocpo_a3,diag_ocpo_a4,diag_bcpi_a1,diag_bcpi_a2,diag_bcpi_a3,diag_bcpi_a4,diag_bcpo_a1,diag_bcpo_a2,diag_bcpo_a3,diag_bcpo_a4,diag_seas_a1,diag_seas_a2,diag_seas_a3,diag_seas_a4,diag_dst_a1,diag_dst_a2,diag_dst_a3,diag_dst_a4,diag_soas_a1,diag_soas_a2,diag_soas_a3,diag_soas_a4,diag_so4_cw1,diag_so4_cw2,diag_so4_cw3,diag_so4_cw4,diag_nit_cw1,diag_nit_cw2,diag_nit_cw3,diag_nit_cw4,diag_nh4_cw1,diag_nh4_cw2,diag_nh4_cw3,diag_nh4_cw4,diag_ocpi_cw1,diag_ocpi_cw2,diag_ocpi_cw3,diag_ocpi_cw4,diag_ocpo_cw1,diag_ocpo_cw2,diag_ocpo_cw3,diag_ocpo_cw4,diag_bcpi_cw1,diag_bcpi_cw2,diag_bcpi_cw3,diag_bcpi_cw4,diag_bcpo_cw1,diag_bcpo_cw2,diag_bcpo_cw3,diag_bcpo_cw4,diag_seas_cw1,diag_seas_cw2,diag_seas_cw3,diag_seas_cw4,diag_dst_cw1,diag_dst_cw2,diag_dst_cw3,diag_dst_cw4,diag_soas_cw1,diag_soas_cw2,diag_soas_cw3,diag_soas_cw4,diag_water_a1,diag_water_a2,diag_water_a3,diag_water_a4,diag_num_a1,diag_num_a2,diag_num_a3,diag_num_a4,diag_num_cw1,diag_num_cw2,diag_num_cw3,diag_num_cw4"
+
+# Starting in WRF-GC v3.0 (WRF v4 + GEOS-Chem v13.4.1/v14.0.0), GEOS-Chem species
+# names will directly be used in WRF after a lowercase processing. This is to
+# resolve many lingering headaches. (hplin, 8/11/22)
+
+############ NO USER CONFIGURABLE CODE BELOW ############
 
 if parser_mode == "regex":
     with open(input_file_name, 'r') as file:
@@ -47,7 +93,7 @@ elif parser_mode == "yaml":
                 # heap alloc.
                 parsed_spc_list.append((N, spcName, Is_Advected, Is_DryDep, Is_WetDep, Mw_g))
 
-            print(parsed_spc_list)
+            # print(parsed_spc_list)
         except yaml.YAMLError as exc:
             print(exc)
 else:
@@ -65,45 +111,10 @@ idx_DryDep = 3
 idx_WetDep = 4
 idx_Mw_g = 5
 
-# cesm | wrfgc
-mode = "cesm"
-
 # sort by adv to keep them first - even though GC should keep them first
 # this is for sanity.
 parsed_spc_list.sort(key = lambda x: 0 if x[idx_Advect] == "T" else 1)
 
-#
-# These are "quirky" species, which necessitate special handling
-# in coupled models such as WRF and CESM.
-#
-
-# Non-gas-phase species ("Aerosols")
-nongas = ["AERI", "ASOAN", "ASOA1", "ASOA2", "ASOA3", "ASOG1", "ASOG2", "ASOG3", "AONITA", "BCPI", "BCPO", "BrSALA", "BrSALC", "DMS", "DST1", "DST2", "DST3", "DST4", "INDIOL", "IONITA", "ISALA", "ISALC", "ISN1OA", "ISN1OG", "ISOA1", "ISOA2", "ISOA3", "MONITA", "MSA", "NH4", "NIT", "NITs", "OCPI", "OCPO", "OPOA1", "OPOA2", "POA1", "POA2", "SALA", "SALC", "SALACl", "SALAAL", "SALCAL", "SALCCl", "SO4", "SO4s", "SOAIE", "SOAGX", "SOAME", "SOAMG", "SOAP", "SOAS", "TSOA0", "TSOA1", "TSOA2", "TSOA3", "TSOG0", "TSOG1", "TSOG2", "TSOG3", "pFe", "POA1", "POA2", "POG1", "POG2"]
-
-###################
-#  --- CESM ---   #
-###################
-# CESM has a few fixed species that need to be written to inv(ariants) list
-# from gckpp_Parameters.F90, this is H2, N2, O2, RCOOH
-cesm_invariants = ["H2", "N2", "O2", "RCOOH"]
-
-# CESM-GC requires extra species for aerosols
-cesm_aer = ['bc_a1','bc_a4','dst_a1','dst_a2','dst_a3','ncl_a1','ncl_a2','ncl_a3','num_a1','num_a2','num_a3','num_a4','pom_a1','pom_a4','so4_a1','so4_a2','so4_a3','soa1_a1','soa1_a2','soa2_a1','soa2_a2','soa3_a1','soa3_a2','soa4_a1','soa4_a2','soa5_a1','soa5_a2','H2SO4','SOAG0','SOAG1','SOAG2','SOAG3','SOAG4']
-cesm_aer_mass = [12.011000, 12.011000, 135.064039, 135.064039, 135.064039, 58.442468, 58.442468, 58.442468, 1.007400, 1.007400, 1.007400, 1.007400, 12.011000, 12.011000, 115.107340, 115.107340, 115.107340, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000, 98.078400, 250.445000, 250.445000, 250.445000, 250.445000, 250.445000]
-
-###################
-# --- WRF-GC ---  #
-###################
-# WRF requires non-gas species to be separated into the end. use nongas.
-
-# WRF-GC v2.0+ (Feng et al., 2021) also requires diagnostic bins for aerosols
-wrf_extra_coupling = "diag_so4_a1,diag_so4_a2,diag_so4_a3,diag_so4_a4,diag_nit_a1,diag_nit_a2,diag_nit_a3,diag_nit_a4,diag_nh4_a1,diag_nh4_a2,diag_nh4_a3,diag_nh4_a4,diag_ocpi_a1,diag_ocpi_a2,diag_ocpi_a3,diag_ocpi_a4,diag_ocpo_a1,diag_ocpo_a2,diag_ocpo_a3,diag_ocpo_a4,diag_bcpi_a1,diag_bcpi_a2,diag_bcpi_a3,diag_bcpi_a4,diag_bcpo_a1,diag_bcpo_a2,diag_bcpo_a3,diag_bcpo_a4,diag_seas_a1,diag_seas_a2,diag_seas_a3,diag_seas_a4,diag_dst_a1,diag_dst_a2,diag_dst_a3,diag_dst_a4,diag_soas_a1,diag_soas_a2,diag_soas_a3,diag_soas_a4,diag_so4_cw1,diag_so4_cw2,diag_so4_cw3,diag_so4_cw4,diag_nit_cw1,diag_nit_cw2,diag_nit_cw3,diag_nit_cw4,diag_nh4_cw1,diag_nh4_cw2,diag_nh4_cw3,diag_nh4_cw4,diag_ocpi_cw1,diag_ocpi_cw2,diag_ocpi_cw3,diag_ocpi_cw4,diag_ocpo_cw1,diag_ocpo_cw2,diag_ocpo_cw3,diag_ocpo_cw4,diag_bcpi_cw1,diag_bcpi_cw2,diag_bcpi_cw3,diag_bcpi_cw4,diag_bcpo_cw1,diag_bcpo_cw2,diag_bcpo_cw3,diag_bcpo_cw4,diag_seas_cw1,diag_seas_cw2,diag_seas_cw3,diag_seas_cw4,diag_dst_cw1,diag_dst_cw2,diag_dst_cw3,diag_dst_cw4,diag_soas_cw1,diag_soas_cw2,diag_soas_cw3,diag_soas_cw4,diag_water_a1,diag_water_a2,diag_water_a3,diag_water_a4,diag_num_a1,diag_num_a2,diag_num_a3,diag_num_a4,diag_num_cw1,diag_num_cw2,diag_num_cw3,diag_num_cw4"
-
-# WRF also requires mapping of species to WRF-Chem names for
-# compatibility. TODO hplin 7/20/22
-
-
-############ NO USER CONFIGURABLE CODE BELOW ############
 assert len(cesm_aer) == len(cesm_aer_mass)
 nongas_upper = list(map(lambda x: x.upper(), nongas))
 
@@ -139,7 +150,7 @@ if mode == "cesm":
             wetdep_list += "'" + spc[idx_spcName].upper() + "',"
         
         # insert aerosols in between T and F. check for first F
-        if first_nonadv and spc[2] == "F":
+        if first_nonadv and spc[idx_Advect] == "F":
             first_nonadv = False
             nadv_counter = total_counter + len(cesm_aer) # ...save up to now
             # insert cesm_aer, cesm_aer_mass ...
@@ -181,3 +192,131 @@ if mode == "cesm":
     print("update chem_mods.F90: gas_pcnst = " + str(total_counter))
     print("update chem_mods.F90: nTracersMax = " + str(nadv_counter+1))
     print("update bld/configure: $chem_nadv = " + str(nadv_counter+1))
+
+elif mode == "wrfgc" or mode == "wrf":
+
+    # WRF format... lots to generate.
+    # registry.chem
+    registry_chem_lines = "state   real   -           ikjftb   chem        1         -   -                             -\n"
+    registry_chem_gas = ""
+    registry_chem_nongas = ""
+
+    # gigc_set_wrf (GC -> WRF, update all entries)
+    gigc_set_wrf_nocomplexsoa = ""
+    gigc_set_wrf_complexsoa = ""
+    gigc_set_wrf_complexsoa_svpoa = ""
+
+    # gigc_get_wrf (WRF -> GC, only advected)
+    gigc_get_wrf_nocomplexsoa = ""
+    gigc_get_wrf_complexsoa = ""
+    gigc_get_wrf_complexsoa_svpoa = ""
+
+    # idxarray
+    gigc_idxarray = "integer :: "
+
+    # lookup code
+    gigc_idxlookup = ""
+
+    # debug output
+    gigc_idxdebug = ""
+
+    # for WRF-GC, for prettiness, the parsed_spc_list is sorted first
+    parsed_spc_list.sort(key = lambda x: x[idx_spcName])
+
+    for spc_idx, spc in enumerate(parsed_spc_list):
+        wrfName = spc[idx_spcName].lower()
+        gcName = spc[idx_spcName]
+
+        spcName_quoted = "\"" + wrfName + "\""
+        spcName_desc_quoted = "\"" + gcName + " concentration\""
+
+        if is_gc_14_and_above:
+            # use new State_Chm%Species(id_Hg2)%Conc(:,:,:) format
+            get_spec = "State_Chm%Species(gi_" + wrfName + ")%Conc(II, JJ, k) = chem(i, k, j, p_" + wrfName + ") * 1.0e-6_fp\n"
+            set_spec = "chem(i, k, j, p_" + wrfName + ") = State_Chm%Species(gi_" + wrfName + ")%Conc(II, JJ, k) * 1.0e+6_fp\n"
+        else:
+            get_spec = "State_Chm%Species(II, JJ, k, gi_" + wrfName + ") = chem(i, k, j, p_" + wrfName + ") * 1.0e-6_fp\n"
+            set_spec = "chem(i, k, j, p_" + wrfName + ") = State_Chm%Species(II, JJ, k, gi_" + wrfName + ") * 1.0e+6_fp\n"
+
+        if spc[idx_Advect] == "T":
+            registry_chem_lines += "state   real   " + wrfName.ljust(10) + "  ikjftb   chem        1         -   i0{12}rhusdf=(bdy_interp:dt)  " + spcName_quoted.ljust(14) + " " + spcName_desc_quoted.ljust(27) + " \"ppmv\"\n"
+
+            # add to gigc_get_wrf
+            if gcName in complexsoa:
+                gigc_get_wrf_complexsoa += "    " + get_spec
+            elif gcName in complexsoa_svpoa:
+                gigc_get_wrf_complexsoa_svpoa += "    " + get_spec
+            else:
+                gigc_get_wrf_nocomplexsoa += get_spec
+
+        else:
+            registry_chem_lines += "state   real   " + wrfName.ljust(10) + "  ikjft    chem        1         -         rhusdf=(bdy_interp:dt)  " + spcName_quoted.ljust(14) + " " + spcName_desc_quoted.ljust(27) + " \"ppmv\"\n"
+
+        # check if gas / nongas
+        if spc[idx_spcName] in nongas:
+            # non-gas
+            registry_chem_nongas += wrfName + ","
+        else:
+            # gas
+            registry_chem_gas += wrfName + ","
+
+        # add to idx lookup
+        gigc_idxarray    += "gi_" + wrfName + ","
+        gigc_idxlookup   += "gi_" + wrfName + " = IND_('" + gcName + "')\n"
+        gigc_idxdebug    += "write(6,*) p_" + wrfName + ", \"" + wrfName + " = " + gcName + "\", gi_" + wrfName + "\n"
+
+
+        # add to gigc_set_wrf
+        if gcName in complexsoa:
+            gigc_set_wrf_complexsoa += "    " + set_spec
+        elif gcName in complexsoa_svpoa:
+            gigc_get_wrf_complexsoa_svpoa += "    " + set_spec
+        else:
+            gigc_set_wrf_nocomplexsoa += set_spec
+
+    # Do some post-processing of the complexSOA code ...
+    gigc_set_wrf_complexsoa       = "if(Input_Opt%LSOA) then\n" + gigc_set_wrf_complexsoa + "endif\n"
+    gigc_set_wrf_complexsoa_svpoa = "if(Input_Opt%LSVPOA) then\n" + gigc_set_wrf_complexsoa_svpoa + "endif\n"
+
+    gigc_get_wrf_complexsoa       = "if(Input_Opt%LSOA) then\n" + gigc_get_wrf_complexsoa + "endif\n"
+    gigc_get_wrf_complexsoa_svpoa = "if(Input_Opt%LSVPOA) then\n" + gigc_get_wrf_complexsoa_svpoa + "endif\n"
+
+    # Assemble the final registry entry
+    registry_chem_nongas = "".join(registry_chem_nongas.rsplit(",", 1))
+    registry_chem_gas    = "".join(registry_chem_gas.rsplit(",", 1))
+    gigc_idxarray        = "".join(gigc_idxarray.rsplit(",", 1))
+
+    registry_chem = "package   gchp                  chem_opt==233       -             chem:" + registry_chem_gas + "," + registry_chem_nongas + "," + wrf_extra_coupling
+
+    # Assemble the final get routines in loop...
+    gigc_get_wrf = gigc_get_wrf_nocomplexsoa + "\n\n! Complex SOA species, only if available (hplin, 8/11/22)\n" + gigc_get_wrf_complexsoa + "\n" + gigc_get_wrf_complexsoa_svpoa
+    gigc_set_wrf = gigc_set_wrf_nocomplexsoa + "\n\n! Complex SOA species, only if available (hplin, 8/11/22)\n" + gigc_set_wrf_complexsoa + "\n" + gigc_set_wrf_complexsoa_svpoa
+
+    # write all these to files...
+    f = open("out/wrfgc_convert_state_mod_get.F90", "w")
+    f.write(gigc_get_wrf)
+    f.close()
+
+    f = open("out/wrfgc_convert_state_mod_set.F90", "w")
+    f.write(gigc_set_wrf)
+    f.close()
+
+    f = open("out/wrfgc_convert_state_mod_idxsetup.F90", "w")
+    f.write(gigc_idxlookup + "\n\n" + gigc_idxdebug)
+    f.close()
+
+    f = open("out/wrfgc_convert_state_mod_idxdef.F90", "w")
+    f.write(gigc_idxarray)
+    f.close()
+
+    f = open("out/registry.chem_wrfgc_entries.txt", "w")
+    f.write(registry_chem_lines)
+    f.close()
+
+    print("---- registry.chem chem_opt = 233 entry: ----")
+    print(registry_chem)
+    print("---------------------------------------------")
+    print("Update get_last_gas in module_input_chem_data.F to correspond with the above registry entry.\n")
+
+else:
+    raise ValueError("Unrecognized mode option! Set wrfgc or cesm.")
